32e57be9eea8090d12cd68ef17c9ed45bc814f86
Robin Millette <robin@millette.info>
2019-10-07 13:07:00 -0400

    Basic server.

---
+++ index.js
@@ -0,0 +1,24 @@
+const fastify = require('fastify')({
+  logger: true
+})
+
+fastify.register(require('fastify-formbody'))
+
+// Declare a route
+fastify.get('/', function (request, reply) {
+  reply.type('text/html').send(`<form method="post"><textarea name="cnt"></textarea><button>send
+  `)
+})
+
+fastify.post('/', function (request, reply) {
+  reply.send({ hello: 'world' })
+})
+
+// Run the server!
+fastify.listen(4000, function (err, address) {
+  if (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+  fastify.log.info(`server listening on ${address}`)
+})
