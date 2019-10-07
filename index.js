789753450416332ac26b5342f512a75c3c9d174b
Robin Millette <robin@millette.info>
2019-10-07 16:33:42 -0400

    Use extracted client-side js.

--- index.js
+++ index.js
@@ -1,21 +1,35 @@
+// core
+const { createReadStream, promises: { mkdtemp } } = require('fs')
+
+// npm
 const fastify = require('fastify')({
   logger: true
 })
 
 fastify.register(require('fastify-formbody'))
 
-// Declare a route
-fastify.get('/', function (request, reply) {
-  reply.type('text/html').send(`<form method="post"><textarea name="cnt"></textarea><button>send
-  `)
+const uploadRoot = 'uploaded/'
+
+mkdtemp(uploadRoot)
+.then(console.log)
+.catch(console.error)
+
+// Pas besoin dans charcre, c'est juste un banal textarea dans un form.
+// Tu voudras document.querySelector('svg').innertext ou quelque chose comme ça
+fastify.get('/', async (request, reply) => {
+  reply.type('text/html; charset=utf-8')
+  return `<form method="post"><textarea name="cnt"></textarea><br><button>send</button></form><script src="/main.js"></script>`
 })
 
-fastify.post('/', function (request, reply) {
-  reply.send({ hello: 'world', cnt: request.body.cnt })
+fastify.get('/main.js', async (request, reply) => createReadStream('./client-js/main.js', 'utf8'))
+
+// handle either text/plain or application/json
+fastify.post('/', async (request) => {
+  return { hello: 'world', cnt: typeof request.body === 'string' ? request.body : request.body.cnt }
 })
 
 // Run the server!
-fastify.listen(4000, function (err, address) {
+fastify.listen(4000, (err, address) => {
   if (err) {
     fastify.log.error(err)
     process.exit(1)
