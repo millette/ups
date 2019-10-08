43c8e8532c5014bbd0b797ec25724fa53777015f
Robin Millette <robin@millette.info>
2019-10-08 15:51:34 -0400

    Simplify and add host env var.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 require('dotenv-safe').config()
 const fastify = require('fastify')({ logger: true })
 // only required to process normal browser forms (without ajax)
-fastify.register(require('fastify-formbody'))
+// fastify.register(require('fastify-formbody'))
 
 // core
 const { createReadStream, promises: { mkdtemp, writeFile } } = require('fs')
@@ -13,10 +13,12 @@ const { UPLOAD_PORT, UPLOAD_ROOT_FS, UPLOAD_ROOT_URL } = process.env
 // Pas besoin dans charcre, c'est juste un banal textarea dans un form.
 // Tu voudras document.querySelector('svg').innertext
 // ou quelque chose comme ça.
+/*
 fastify.get('/', async (request, reply) => {
   reply.type('text/html; charset=utf-8')
   return `<form method="post"><textarea name="cnt"></textarea><br><button>send</button></form><script src="/main.js"></script>`
 })
+*/
 
 // This is where svg files are downloaded from
 // the file name (file.svg) could probably be derived
@@ -32,9 +34,10 @@ fastify.get(`${UPLOAD_ROOT_URL}:id/file.svg`, (request, reply) => {
 })
 
 // only needed to demo our form/ajax
-fastify.get('/main.js', async (request, reply) => createReadStream('client-js/main.js', 'utf8'))
+// fastify.get('/main.js', async (request, reply) => createReadStream('client-js/main.js', 'utf8'))
 
 // handle either text/plain or application/json
+/*
 fastify.post('/', async (request) => {
   const cnt = typeof request.body === 'string' ? request.body : request.body.cnt
   if (!cnt) throw new Error('Missing cnt')
@@ -43,9 +46,20 @@ fastify.post('/', async (request) => {
   await writeFile(fn, cnt)
   return { url: `${UPLOAD_ROOT_FS}${fn}`, size: cnt.length }
 })
+*/
+
+fastify.put('/', async (request) => {
+  // const cnt = typeof request.body === 'string' ? request.body : request.body.cnt
+  if (!request.body || typeof request.body !== 'string') throw new Error('Missing cnt')
+  const cnt = request.body
+  const dir = await mkdtemp(UPLOAD_ROOT_FS)
+  const fn = `${dir}/file.svg`
+  await writeFile(fn, cnt)
+  return { url: `${UPLOAD_ROOT_FS}${fn}`, size: cnt.length }
+})
 
 // Run the server!
-fastify.listen(UPLOAD_PORT || 4000, (err, address) => {
+fastify.listen(UPLOAD_PORT || 4000, UPLOAD_HOST || 'localhost', (err, address) => {
   if (err) {
     fastify.log.error(err)
     process.exit(1)
